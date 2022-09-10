41a1b2b4403f8364248af43d379e4b159488737c
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-10 13:52:14 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿void NumbersBetween(int m, int n)
+{
+    Console.Write(m + "  ");
+    ++m;
+    if (m <= n)
+        NumbersBetween(m, n);
+}
+
+Console.WriteLine("Введите значения начала и конца промежутка");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nПромежуток:");
+NumbersBetween(m, n);
